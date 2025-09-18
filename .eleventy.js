d5587b6d27f69a777bfba5e1d8b0b2bca5739c93
miguelms017 <miguelmartinez030205@gmail.com>
2025-09-18 14:57:24 -0400

    to netlify

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig){
     //copy dirs
     eleventyConfig.addPassthroughCopy ("src/images");
     eleventyConfig.addPassthroughCopy ("src/fonts");
+    eleventyConfig.addPassthroughCopy ("src/script");
 
     //dirs
     return{
